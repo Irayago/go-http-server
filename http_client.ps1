1b3578f46aad3b84c88fbd6f8576dc6a0634b8a4
Ira <49924651+Irayago@users.noreply.github.com>
2026-06-17 15:27:57 -0700

    Refactor HTTP requests in http_client.ps1
    Changed to only send 1 HTTP request

--- http_client.ps1
+++ http_client.ps1
@@ -2,7 +2,12 @@
 # Date: 6/4/2026
 # Description: HTTP client
 
-# send 2 HTTP requests through same TCP connection
-for ($i = 0; $i -le 1; $i++) {
-	curl http://10.124.48.77:9999 #will need to change the ip addr to server host ip
+# send $i HTTP requests through same TCP connection
+<#
+for ($i = 0; $i -le 2; $i++) {
+	#curl -v -s http://REPLACE_WITH_IP #will need to change the ip addr to server host ip
+	Invoke-WebRequest -Uri "http://REPLACE_WITH_IP" | Select-Object -ExpandProperty Content
 }
+#>
+
+Invoke-WebRequest -Uri "http://REPLACE_WITH_IP" | Select-Object -ExpandProperty Content
